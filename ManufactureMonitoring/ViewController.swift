c6066060cc6f3e5691b89c5102aca7e34e5b9237
Alevtina Mashtaler <alya.mashtalez@gmail.com>
2024-11-02 13:11:13 +0300

    Initial Commit

---
+++ ManufactureMonitoring/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ManufactureMonitoring
+//
+//  Created by Admin on 02.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
