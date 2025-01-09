954dfdd1caec4886ee6b12e735216098caa3021d
Alevtina Mashtaler <alya.mashtalez@gmail.com>
2025-01-09 13:41:57 +0300

    Colors struct added

---
+++ ManufactureMonitoring/Resources/Colors.swift
@@ -0,0 +1,17 @@
+//
+//  Colors.swift
+//  ManufactureMonitoring
+//
+//  Created by Admin on 09.01.2025.
+//
+
+import UIKit
+
+struct Colors {
+    
+    static let grey = UIColor(red: 85/255, green: 91/225, blue: 110/255, alpha: 1)
+    static let lightBlue = UIColor(red: 190/255, green: 227/225, blue: 219/255, alpha: 0.6)
+    static let blue = UIColor(red: 137/255, green: 176/225, blue: 174/255, alpha: 1)
+    static let background = UIColor(red: 250/255, green: 249/225, blue: 249/255, alpha: 1)
+    static let sand = UIColor(red: 255/255, green: 214/225, blue: 186/255, alpha: 1)
+}
