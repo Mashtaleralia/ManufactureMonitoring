55316fd1fcbf0e2d7e6312c40d81439a60fef17c
Alevtina Mashtaler <alya.mashtalez@gmail.com>
2025-01-09 19:32:03 +0300

    adding collection view instead of tableview

--- ManufactureMonitoring/Views/DetailsView.swift
+++ ManufactureMonitoring/Views/DetailsView.swift
@@ -21,20 +21,22 @@ class DetailsView: UIView, DetailsViewDelegate {
     
     weak var delegate: DetailInfoViewDelegate?
     
-    private var tableView: UITableView = {
-        let tableView = UITableView()
-        tableView.register(DetailTableViewCell.self, forCellReuseIdentifier: DetailTableViewCell.identifier)
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.backgroundColor = .systemBackground
-        return tableView
+    public var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(DetailCollectionViewCell.self, forCellWithReuseIdentifier: DetailCollectionViewCell.identifier)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = Colors.lightBlue
+        return collectionView
     }()
+    
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        tableView.delegate = self
-        tableView.dataSource = self
+        collectionView.delegate = self
+        collectionView.dataSource = self
         translatesAutoresizingMaskIntoConstraints = false
-        addSubview(tableView)
+        addSubview(collectionView)
         setUpConstraints()
     }
     
@@ -45,42 +47,84 @@ class DetailsView: UIView, DetailsViewDelegate {
     private func setUpConstraints() {
         
         NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: topAnchor),
-            tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
-            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
-            tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            collectionView.topAnchor.constraint(equalTo: topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
         ])
         
     }
     
 }
 
-extension DetailsView: UITableViewDelegate, UITableViewDataSource {
+extension DetailsView: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        presenter.numberOfDetails
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if presenter.isSearching {
+            return presenter.resultsCount
+        } else {
+            return presenter.numberOfDetails
+        }
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: DetailTableViewCell.identifier, for: indexPath) as? DetailTableViewCell else {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: DetailCollectionViewCell.identifier, for: indexPath) as? DetailCollectionViewCell else {
             fatalError()
         }
-        cell.accessoryType = .detailDisclosureButton
-        cell.configure(with: presenter.detailAtIndex(indexPath.row))
+        if presenter.isSearching {
+            cell.configure(with: presenter.detailAtSearchResultByIndex(indexPath.row))
+        } else {
+            cell.configure(with: presenter.detailAtIndex(indexPath.row))
+        }
+        
         return cell
     }
     
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        UIScreen.main.bounds.height / 8
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        let detail = presenter.detailAtIndex(indexPath.row)
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        var detail: Detail
+        if presenter.isSearching {
+            detail = presenter.detailAtSearchResultByIndex(indexPath.row)
+        } else {
+           detail = presenter.detailAtIndex(indexPath.row)
+        }
+        //let detail = presenter.detailAtIndex(indexPath.row)
         let presenter = DetailInfoViewPresenter(detail: detail)
         let vc = DetailInfoViewController(presenter: presenter)
         vc.navigationController?.navigationBar.prefersLargeTitles = true
         delegate?.presentVC(vc)
     }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let padding = 15
+        return CGSize(width: frame.width-CGFloat(2*padding), height: frame.height/10)
+    }
+    
+    
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        presenter.numberOfDetails
+//    }
+//    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        guard let cell = tableView.dequeueReusableCell(withIdentifier: DetailCollectionViewCell.identifier, for: indexPath) as? DetailCollectionViewCell else {
+//            fatalError()
+//        }
+//       
+//        cell.configure(with: presenter.detailAtIndex(indexPath.row))
+//        return cell
+//    }
+//    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        UIScreen.main.bounds.height / 8
+    }
+    
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        tableView.deselectRow(at: indexPath, animated: true)
+//        let detail = presenter.detailAtIndex(indexPath.row)
+//        let presenter = DetailInfoViewPresenter(detail: detail)
+//        let vc = DetailInfoViewController(presenter: presenter)
+//        vc.navigationController?.navigationBar.prefersLargeTitles = true
+//        delegate?.presentVC(vc)
+//    }
     
 }
