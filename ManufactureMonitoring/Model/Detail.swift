7059abe394d65bf4c51f94433d1e02d2e25084c0
Alevtina Mashtaler <alya.mashtalez@gmail.com>
2025-01-09 13:43:32 +0300

    Detail entity created

---
+++ ManufactureMonitoring/Model/Detail.swift
@@ -0,0 +1,22 @@
+//
+//  Detail.swift
+//  ManufactureMonitoring
+//
+//  Created by Admin on 04.11.2024.
+//
+
+import Foundation
+
+struct Detail {
+    var Name: String
+    var id: String
+    var id_2: Int64
+    var id_3: Int64
+    var parent: Int64
+    var dur: Int64
+    var t_sht: Double
+    var t_pz: Double
+    var party: Double
+    var _tr: String
+    var twenty: String
+}
