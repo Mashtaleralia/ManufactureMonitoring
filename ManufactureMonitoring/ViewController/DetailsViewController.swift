dd31b42937e780b61be182885bf30a4b971bf9b7
Alevtina Mashtaler <alya.mashtalez@gmail.com>
2025-01-09 19:46:17 +0300

    dismissing keyboard when tapped outside of searchbar

--- ManufactureMonitoring/ViewController/DetailsViewController.swift
+++ ManufactureMonitoring/ViewController/DetailsViewController.swift
@@ -41,6 +41,7 @@ class DetailsViewController: UIViewController {
         navigationItem.titleView = searchBar
         print(DBHelper.shared.details(name: "Пята"))
         navigationController?.navigationBar.prefersLargeTitles = true
+        hideKeyboardWhenTappedAround()
     }
     
     private func searchByName(_ text: String) {
@@ -78,3 +79,19 @@ extension DetailsViewController: DetailInfoViewDelegate {
     }
 }
 
+extension UIViewController {
+
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard(_:)))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+
+    @objc func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+
+        if let nav = self.navigationController {
+            nav.view.endEditing(true)
+        }
+    }
+ }
