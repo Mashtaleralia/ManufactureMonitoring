d09e133836e4a569de5f143a704e78db4dc11110
Alevtina Mashtaler <alya.mashtalez@gmail.com>
2025-01-09 19:44:37 +0300

    search logic methods added

--- ManufactureMonitoring/Presenter/DetailsViewPresenter.swift
+++ ManufactureMonitoring/Presenter/DetailsViewPresenter.swift
@@ -13,11 +13,14 @@ protocol DetailsViewPresenterProtocol {
 
 class DetailsViewPresenter: DetailsViewPresenterProtocol {
     
-    private var detailView: DetailsViewDelegate
+    private var detailView: DetailsView
     private var details: [Detail]
     private var dbService = DBHelper.shared
+    private var searchResults = [Detail]()
     
-    init(view: DetailsViewDelegate) {
+    var isSearching = false
+    
+    init(view: DetailsView) {
         self.detailView = view
         self.details = dbService.fetchAllDetails()
     }
@@ -26,13 +29,33 @@ class DetailsViewPresenter: DetailsViewPresenterProtocol {
         return details.count
     }
     
+    var resultsCount: Int {
+        return searchResults.count
+    }
+    
     func fetchDetails() {
         self.details = dbService.fetchAllDetails()
         
     }
     
+    func getSearchResults() -> [Detail] {
+        return searchResults
+    }
+    
+    func detailAtSearchResultByIndex(_ index: Int) -> Detail {
+        return searchResults[index]
+    }
+    
     func detailAtIndex(_ index: Int) -> Detail {
         return details[index]
+
+    }
+    
+    func performSearch(by name: String) {
+        isSearching = true
+        self.searchResults = dbService.details(name: name)
+        detailView.collectionView.reloadData()
+        //isSearching = false
     }
     
     
