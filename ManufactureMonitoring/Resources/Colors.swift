152efbcd0cc41365e601a255fb47e852e52230f5
Alevtina Mashtaler <alya.mashtalez@gmail.com>
2025-01-09 19:27:13 +0300

    colors update

--- ManufactureMonitoring/Resources/Colors.swift
+++ ManufactureMonitoring/Resources/Colors.swift
@@ -10,6 +10,7 @@ import UIKit
 struct Colors {
     
     static let grey = UIColor(red: 85/255, green: 91/225, blue: 110/255, alpha: 1)
+    static let lightGrey = UIColor(red: 85/255, green: 91/225, blue: 110/255, alpha: 0.3)
     static let lightBlue = UIColor(red: 190/255, green: 227/225, blue: 219/255, alpha: 0.6)
     static let blue = UIColor(red: 137/255, green: 176/225, blue: 174/255, alpha: 1)
     static let background = UIColor(red: 250/255, green: 249/225, blue: 249/255, alpha: 1)
