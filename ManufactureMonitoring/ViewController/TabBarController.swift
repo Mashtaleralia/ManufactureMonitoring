5b1527a92cd2cb6be9589a237ac819a38d14e1fd
Alevtina Mashtaler <alya.mashtalez@gmail.com>
2025-01-18 20:43:48 +0300

    added TabBarController

---
+++ ManufactureMonitoring/ViewController/TabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarController.swift
+//  ManufactureMonitoring
+//
+//  Created by Admin on 18.01.2025.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
