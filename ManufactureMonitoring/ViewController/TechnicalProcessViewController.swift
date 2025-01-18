65ff06f4079627ee446f4eaae8f8965707b63cc5
Alevtina Mashtaler <alya.mashtalez@gmail.com>
2025-01-18 21:59:37 +0300

    technicalProcessViewController

---
+++ ManufactureMonitoring/ViewController/TechnicalProcessViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TechnicalProcessViewController.swift
+//  ManufactureMonitoring
+//
+//  Created by Admin on 18.01.2025.
+//
+
+import UIKit
+
+class TechnicalProcessViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
