eb01f42accb1e2c7d6268ddb13844617822676f7
Alevtina Mashtaler <alya.mashtalez@gmail.com>
2025-01-18 21:55:34 +0300

    setting tabs

--- ManufactureMonitoring/ViewController/TabBarController.swift
+++ ManufactureMonitoring/ViewController/TabBarController.swift
@@ -11,7 +11,26 @@ class TabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        //UITabBar.appearance().barTintColor = Colors.grey
+        setTabs()
+    }
+    
+    private func setTabs() {
+        let detailView = DetailsView()
+        let presenter = DetailsViewPresenter(view: detailView)
+        detailView.presenter = presenter
+        let detailsVC = DetailsViewController(detailView: detailView, presenter: presenter)
+        
+        let manufactoringProcessVC = UIViewController()
+        
+        let nav1 = UINavigationController(rootViewController: detailsVC)
+        let nav2 = UINavigationController(rootViewController: manufactoringProcessVC)
+       
+        nav1.tabBarItem = UITabBarItem(title: "Детали", image: UIImage(systemName: "gearshape"), tag: 1)
+        nav2.tabBarItem = UITabBarItem(title: "Тех. процесс", image: UIImage(systemName: "repeat"), tag: 2)
+        
+        
+        setViewControllers([nav1, nav2], animated: false)
     }
 
 
